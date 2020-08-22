ced1998d7575da95fa89eb3ec26ebb7f1f8fcf08
Kristin Barr <kristinbarr@gmail.com>
2020-08-22 16:19:57 -0400

    plants and waterings migration

---
+++ data/migrations/20200822151833_plants-and-waterings.js
@@ -0,0 +1,24 @@
+exports.up = async knex => {
+  await knex.schema.createTable('plants', tbl => {
+    tbl.increments()
+    tbl.text('name').notNullable()
+    tbl.text('variety')
+  })
+  await knex.schema.createTable('waterings', tbl => {
+    tbl.increments()
+    tbl.timestamp('created_at', ).notNullable()
+    tbl
+      .integer('plant_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('plants')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+  })
+}
+
+exports.down = async knex => {
+  await knex.schema.dropTableIfExists('waterings')
+  await knex.schema.dropTableIfExists('plants')
+}
