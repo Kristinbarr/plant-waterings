a336ab5cce7ee92ed563a6c1f1631df5c24725df
Kristin Barr <kristinbarr@gmail.com>
2020-08-23 14:02:45 -0400

    seeds update, more waterings

--- data/seeds/01-plants.js
+++ data/seeds/01-plants.js
@@ -5,12 +5,12 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('plants').insert([
-        {id: 1, name: 'Tomato', variety: 'Beefsteak'},
-        {id: 2, name: 'Tomato', variety: 'Red Cherry'},
-        {id: 3, name: 'Strawberry', variety: 'June-bearing'},
-        {id: 4, name: 'Fig', variety: 'Brown Turkey'},
-        {id: 5, name: 'Basil', variety: 'Sweet'},
-        {id: 6, name: 'Aloe', variety: 'Vera'}
+        {name: 'Tomato', variety: 'Beefsteak'},
+        {name: 'Tomato', variety: 'Red Cherry'},
+        {name: 'Strawberry', variety: 'June-bearing'},
+        {name: 'Fig', variety: 'Brown Turkey'},
+        {name: 'Basil', variety: 'Sweet'},
+        {name: 'Aloe', variety: 'Vera'}
       ]);
     });
 };
--- data/seeds/02-waterings.js
+++ data/seeds/02-waterings.js
@@ -5,11 +5,14 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('waterings').insert([
-        {id: 1, created_at: '2020-08-19T19:01:47.937Z', plant_id: 1},
-        {id: 2, created_at: '2020-08-20T19:02:02.976Z', plant_id: 2},
-        {id: 3, created_at: '2020-08-21T19:03:44.472Z', plant_id: 3},
-        {id: 4, created_at: '2020-08-22T19:03:54.623Z', plant_id: 4},
-        {id: 5, created_at: '2020-08-23T19:04:03.738Z', plant_id: 5}
+        {created_at: '2020-08-19T19:01:47.937Z', plant_id: 1},
+        {created_at: '2020-08-19T19:01:47.937Z', plant_id: 1},
+        {created_at: '2020-08-20T19:02:02.976Z', plant_id: 2},
+        {created_at: '2020-08-20T19:02:02.976Z', plant_id: 2},
+        {created_at: '2020-08-21T19:03:44.472Z', plant_id: 3},
+        {created_at: '2020-08-21T19:03:44.472Z', plant_id: 3},
+        {created_at: '2020-08-22T19:03:54.623Z', plant_id: 4},
+        {created_at: '2020-08-23T19:04:03.738Z', plant_id: 5}
       ]);
     });
 };
