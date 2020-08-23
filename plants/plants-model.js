e7c248b599f25a66a1e1cc52685c0dcb3038633f
Kristin Barr <kristinbarr@gmail.com>
2020-08-22 22:12:00 -0400

    add plants model

---
+++ plants/plants-model.js
@@ -0,0 +1,39 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+  find,
+  findById,
+  insert,
+  update,
+  remove,
+};
+
+function find() {
+  return db('plants');
+}
+
+function findById(id) {
+  return db('plants')
+    .where({ id })
+    .first();
+}
+
+function insert(plant) {
+  return db('plants')
+    .insert(plant)
+    .then(ids => {
+      return getById(ids[0]);
+    });
+}
+
+function update(id, changes) {
+  return db('plants')
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+  return db('plants')
+    .where('id', id)
+    .del();
+}
