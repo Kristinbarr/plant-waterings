d1e033e8306d41632c994de03d310f449db5a4ee
Kristin Barr <kristinbarr@gmail.com>
2020-08-26 00:17:34 -0400

    tracer init WIP

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 require('dotenv').config()
 
+const tracer = require('dd-trace').init()
+
+tracer.init({
+  analytics: true,
+  profiling: true,
+})
+
+tracer.use('pg', {
+  analytics: true
+})
+
 const server = require('./server')
 const PORT = process.env.PORT || 3001
 
