607adccc5a64b22ba3baccb25976dc649b4269be
Kristin Barr <kristinbarr@gmail.com>
2020-08-29 23:43:58 -0400

    changed port

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ tracer.init({
 var StatsD = require('hot-shots')
 var dogstatsd = new StatsD()
 dogstatsd.increment('plant-waterings-index')
-dogstatsd.histogram('my_histogram', 42)
+// dogstatsd.histogram('my_histogram', 42)
 
 const server = require('./server')
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
   console.log(`\n*** Listening on port: ${PORT} ***\n`)
