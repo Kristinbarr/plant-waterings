d4e809f3d19463848dd5df9eb3907932a306df45
Kristin Barr <kristinbarr@gmail.com>
2020-08-31 23:45:54 -0400

    commented out winston file

--- winston.js
+++ winston.js
@@ -1,16 +1,16 @@
-const { createLogger, format, transports } = require('winston');
+// const { createLogger, format, transports } = require('winston');
 
-const logger = createLogger({
-  level: 'info',
-  exitOnError: false,
-  format: format.json(),
-  transports: [
-    new transports.File({ filename: `logs/waterings-log.log` }),
-  ],
-});
+// const logger = createLogger({
+//   level: 'info',
+//   exitOnError: false,
+//   format: format.json(),
+//   transports: [
+//     new transports.File({ filename: `logs/waterings-log.log` }),
+//   ],
+// });
 
-module.exports = logger;
+// module.exports = logger;
 
-// Example logs
-logger.log('info', 'Hello simple log!');
-logger.info('Hello log with metas',{color: 'blue' });
+// // Example logs
+// logger.log('info', 'Hello simple log!');
+// logger.info('Hello log with metas',{color: 'blue' });
